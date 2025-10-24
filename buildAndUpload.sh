12596ead8da2c7a54b0eea6453546a4ed2fabc92
Daniil Sharko <sharko@tbdd.ru>
2025-10-24 14:17:02 +0500

    фикс скрипта заливки на сервер

--- buildAndUpload.sh
+++ buildAndUpload.sh
@@ -2,9 +2,9 @@
 echo "====== Сборка проекта ======" &&
 vite build &&
 echo "\e[31m====== Очистка директории перед заливкой ======\e[0m"
-ssh scronheim@dark-corner.ru rm -rf /srv/metal-library/front/assets
+ssh root@metal-library.ru rm -rf /srv/metal-library/front/assets
 echo "====== Заливка файлов ======"
-scp -prq ./dist/* "root@64.188.73.107":/srv/metal-library/front
+scp -prq ./dist/* "root@metal-library.ru":/srv/metal-library/front
 echo "\e[32m====== Заливка файлов завершена ======\e[0m"
 wait &&
 echo "====== Удаление сборочных артефактов ======" &&
