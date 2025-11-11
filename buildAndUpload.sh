bd17584052f1246e02360de8f7680903e5180531
Scronheim <sharko@tbdd.ru>
2025-11-11 14:33:27 +0500

    всякие фиксы

--- buildAndUpload.sh
+++ buildAndUpload.sh
@@ -2,9 +2,9 @@
 echo "====== Сборка проекта ======" &&
 vite build &&
 echo "\e[31m====== Очистка директории перед заливкой ======\e[0m"
-ssh root@metal-archives.ru rm -rf /srv/metal-library/front/assets
+ssh root@metal-archives.ru rm -rf /srv/metal-archives/front/assets
 echo "====== Заливка файлов ======"
-scp -prq ./dist/* "root@metal-archives.ru":/srv/metal-library/front
+scp -prq ./dist/* "root@metal-archives.ru":/srv/metal-archives/front
 echo "\e[32m====== Заливка файлов завершена ======\e[0m"
 wait &&
 echo "====== Удаление сборочных артефактов ======" &&
